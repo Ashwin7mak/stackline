33dee26237e760dc8416696ea9ba20a3dbf31b36
Kyle <k.yasumiishi@gmail.com>
2018-08-05 12:17:59 -0700

    Added fetchData

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -1,5 +1,6 @@
 import { SORT, FETCH } from "../../constants";
 import axios from "axios";
+// import fetch from "node-fetch";
 
 export const sortBy = key => {
   return {
@@ -8,22 +9,28 @@ export const sortBy = key => {
   };
 };
 
-export const fetch = data => {
+export const fetchAction = data => {
   return {
     type: FETCH,
     data
   };
 };
 
-const ax = axios.create({
-  baseURL: "http://localhost:3001/"
-});
-
 export function fetchData() {
   return dispatch => {
-    return ax.get("Webdev_data.json").then(data => {
-      console.log(data);
-      dispatch(fetch(data[0]));
+    return axios.get("https://api.myjson.com/bins/126og8").then(data => {
+      console.log(JSON.parse(data.request.response));
+      const json = JSON.parse(data.request.response);
+      return dispatch(fetchAction(json[0]));
     });
   };
 }
+
+// export function fetchData() {
+//   return dispatch => {
+//     fetch("../../Webdev_data.json").then(data => {
+//       console.log(data);
+//       dispatch(fetchAction(data[0]));
+//     });
+//   };
+// }
